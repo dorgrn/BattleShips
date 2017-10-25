99c1ec7e269232a9e9c3806f612abf437145fb89
Dor Grinshpan <dorgrin6@gmail.com>
2017-10-25 22:07:09 +0300

    merges

--- BattleShipsWebApp/web/pages/game/game.js
+++ BattleShipsWebApp/web/pages/game/game.js
@@ -1,4 +1,4 @@
-const INTERVAL_LENGTH = 5000;
+const INTERVAL_LENGTH = 1000;
 
 const HIT_SYMBOL = 'H';
 const MISS_SYMBOL = 'L';
@@ -12,7 +12,6 @@ $(window).on('load', function () {
     startGame();
 
     intervalRefreshLists = setInterval(function () {
-        // add any other updates you want here...
         ajaxGetMessagesFromServer();
         ajaxUpdateCurrentGame();
     }, INTERVAL_LENGTH);
@@ -37,6 +36,7 @@ function ajaxGetCurrentUserName() {
     });
 }
 
+// TODO: function duplication, i'll improve when i'll have the time
 function ajaxGetCurrentUserType() {
     $.ajax({
         url: '/gamesRoom/currentUserType',
@@ -45,6 +45,7 @@ function ajaxGetCurrentUserType() {
         success: function (users) {
             $.each(users || [], function (index, userType) {
                 $("#myPlayerType").text(userType);
+                sessionStorage.setItem("PLAYER_TYPE", userType);
             });
         }
     });
@@ -61,8 +62,20 @@ function ajaxGetAndInitCurrentGame() {
             // console.log("before");
         },
         success: function (game) {
-            //console.log(game);
             initializeGame(game);
+        }
+    });
+}
+
+function ajaxGetCurrentGameAndUpdateBoards(btn) {
+    $.ajax({
+        url: '/game/currentGameRecord',
+        type: 'GET',
+        cache: false,
+        contentType: 'application/json; charset=utf-8',
+        beforeSend: function () {},
+        success: function (game) {
+            generateAttackAndUpdateScreen(game, btn);
         }
     });
 }
@@ -77,7 +90,7 @@ function ajaxUpdateCurrentGame() {
             // console.log("before");
         },
         success: function (gameRecord) {
-            updateDataOnScreen(gameRecord);
+            initializeGame(gameRecord);
         }
     });
 }
@@ -89,11 +102,11 @@ function initializeGame(gameRecord) {
 }
 
 function createBoardsBySize(gameRecord) {
-    createBattleshipBoard(gameRecord);
-    createTrackingBoard(gameRecord);
+    createPrimaryGrid(gameRecord);
+    createTrackingGrid(gameRecord);
 }
 
-function createBattleshipBoard(gameRecord) {
+function createPrimaryGrid(gameRecord) {
     var boardSize = gameRecord.boardSize;
     var i, j, currentChar, idField, classField;
     var multiplyConst = 0.02 * (20 - boardSize) + 1;
@@ -104,11 +117,13 @@ function createBattleshipBoard(gameRecord) {
     for (i = 0; i < boardSize; i++) {
         table += "<tr>";
         for (j = 0; j < boardSize; j++) {
-            idField = i + " " + j;
+            idField = i + " " + j + " trackingGrid " +
+                (document.getElementById("myPlayerName").innerHTML === gameRecord.creator["username"] ? "player1" : "player2");
             currentChar = getCurrentCharFromBoard("primaryGrid", gameRecord, i, j);
             classField = setClassFieldByCurrentCharacter(currentChar);
-            table += "<th style='padding:0'" + ">" + "<button " + "id=\u0022" + idField + "\u0022 class=\u0022" + classField +
-                "\u0022 style=" + paddingField + " disabled" + "></button>" + "</th>";
+            table += "<th style='padding:0'" + ">" + "<button " + "id=\u0022" + idField +
+                "\u0022 ondrop='drop(event,this)'" + " ondragover='allowDrop(event)'" +
+                "\u0022 class=\u0022" + classField + "\u0022 style=" + paddingField + " disabled" + "></button>" + "</th>";
             if (j === boardSize - 1) {
                 table += "</tr>";
             }
@@ -117,7 +132,7 @@ function createBattleshipBoard(gameRecord) {
     document.getElementById("battleshipBoard").innerHTML = table;
 }
 
-function createTrackingBoard(gameRecord) {
+function createTrackingGrid(gameRecord) {
     var boardSize = gameRecord.boardSize;
     var i, j, currentChar, idField, classField;
     var multiplyConst = 0.02 * (20 - boardSize) + 1;
@@ -127,11 +142,11 @@ function createTrackingBoard(gameRecord) {
     for (i = 0; i < boardSize; i++) {
         table += "<tr>";
         for (j = 0; j < boardSize; j++) {
-            idField = i + " " + j;
+            idField = i + " " + j + " trackingGrid " +
+                (document.getElementById("myPlayerName").innerHTML === gameRecord.creator["username"] ? "player1" : "player2");
             currentChar = getCurrentCharFromBoard("trackingGrid", gameRecord, i, j);
             classField = setClassFieldByCurrentCharacter(currentChar);
-            table += "<th style='padding:0'" + ">" + "<button " + "id=\u0022" + idField + "\u0022 class=\u0022" + classField +
-                "\u0022 style=" + paddingField + "></button>" + "</th>";
+            table += addTrackingBoardButton(classField, paddingField, gameRecord, idField);
             if (j === boardSize - 1) {
                 table += "</tr>";
             }
@@ -140,9 +155,27 @@ function createTrackingBoard(gameRecord) {
     document.getElementById("traceBoard").innerHTML = table;
 }
 
+function addTrackingBoardButton(classField, paddingField, gameRecord, idField) {
+    if (classField === "emptyImg" && gameRecord.participants.length === 2 && isItMyTurn(gameRecord)){
+        return ("<th style='padding:0'" + ">" + "<button " + "type=\u0022button\u0022 " + "onclick=\u0022buttonOnClick(this)" +
+            "\u0022 id=\u0022" + idField + "\u0022 class=\u0022" + classField + "\u0022 style=" + paddingField +
+            "></button>" + "</th>");
+    } else {
+        return ("<th style='padding:0'" + ">" + "<button " + "type=\u0022button\u0022 " + "onclick=\u0022buttonOnClick(this)" +
+            "\u0022 id=\u0022" + idField + "\u0022 class=\u0022" + classField + "\u0022 style=" + paddingField +
+            " disabled" + "></button>" + "</th>");
+    }
+}
+
+function isItMyTurn(gameRecord) {
+    var currentPlayer = gameRecord.game.currentPlayer.playerType;
+    var myPlayer = document.getElementById("myPlayerName").innerHTML === gameRecord.creator["username"] ? "PLAYER_ONE" : "PLAYER_TWO";
+    return (currentPlayer === myPlayer);
+}
+
 function getCurrentCharFromBoard(boardType, gameRecord, i, j) {
     if (boardType === "trackingGrid") {
-        if (gameRecord.game.player1.playerType === document.getElementById("myPlayerType").innerHTML) { // If user is player1
+        if (document.getElementById("myPlayerName").innerHTML === gameRecord.creator["username"]){ // if user is player1
             return gameRecord.game.player1.trackingGrid.board[i][j];
         }
         else { // If user is player2
@@ -150,7 +183,7 @@ function getCurrentCharFromBoard(boardType, gameRecord, i, j) {
         }
     }
     else { // If primaryGrid
-        if (gameRecord.game.player1.playerType === document.getElementById("myPlayerType").innerHTML) { // If user is player1
+        if (document.getElementById("myPlayerName").innerHTML === gameRecord.creator["username"]){ // if user is player1
             return gameRecord.game.player1.primaryGrid.board[i][j];
         }
         else { // If user is player2
@@ -174,6 +207,27 @@ function setClassFieldByCurrentCharacter(currentChar) {
     }
 }
 
+function buttonOnClick(btn){
+    ajaxGetCurrentGameAndUpdateBoards(btn); // Calls 'generateAttackAndUpdateScreen' in success
+}
+
+function generateAttackAndUpdateScreen(gameRecord, btn) {
+    ajaxMakeTurn(gameRecord, btn.id);
+}
+
+function ajaxMakeTurn(gameRecord, buttonID) {
+    $.ajax({
+        type: 'POST',
+        url: MAKE_TURN_URI,
+        dataType: 'html',
+        data: { // should match Constants
+            "GAME_NAME": gameRecord.gameName,
+            "BUTTON_ID": buttonID
+        },
+        success: function () { ajaxUpdateCurrentGame(); }
+    });
+}
+
 function updateDataOnScreen(gameRecord) {
     updateStatistics(gameRecord);
     updateGeneralDataLine(gameRecord);
@@ -181,18 +235,74 @@ function updateDataOnScreen(gameRecord) {
     updateWatchersTable(gameRecord.watchers);
     updateCommentsLine(gameRecord.participants.length);
     updateShipsData(gameRecord.game);
+    updateMineButton(gameRecord, gameRecord.game.currentPlayer.amountOfMinesToPlace, gameRecord.participants.length);
+}
+
+function updateMineButton(gameRecord, minesLeft, numOfPlayers) {
+    var btn;
+    if (numOfPlayers === 2){
+        if (isItMyTurn(gameRecord) && minesLeft > 0)
+            btn = "<button id=\u0022insertMineButton\u0022 draggable='true' ondragstart='drag(event)'></button>";
+        else
+            btn = "<button id=\u0022insertMineButton\u0022 style='opacity: 0.5' disabled></button>";
+        document.getElementById("mineButton").innerHTML = btn;
+    }
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev, btn) {
+    ev.preventDefault();
+    ajaxGetCurrentGameAndPlaceMine(btn);
+}
+
+function ajaxGetCurrentGameAndPlaceMine(btn) {
+    $.ajax({
+        url: '/game/currentGameRecord',
+        type: 'GET',
+        cache: false,
+        contentType: 'application/json; charset=utf-8',
+        beforeSend: function () {},
+        success: function (game) {
+            ajaxPlaceMine(game, btn.id);
+        }
+    });
+}
+
+function ajaxPlaceMine(gameRecord, buttonID) {
+    $.ajax({
+        type: 'POST',
+        url: '/gamesRoom/placeMine',
+        dataType: 'html',
+        data: { // should match Constants
+            "GAME_NAME": gameRecord.gameName,
+            "BUTTON_ID": buttonID
+        },
+        success: function () { ajaxUpdateCurrentGame(); }
+    });
 }
 
 function updateStatistics(gameRecord) {
     var currentGame = gameRecord.game;
-    $("#currentPlayer").text(("PLAYER_ONE" === currentGame.currentPlayer.playerType ?
-        gameRecord.creator.username : (gameRecord.participants[0]["username"] === gameRecord.creator.username ?
-            gameRecord.participants[0]["username"] : gameRecord.participants[1]["username"])) + "'s Turn");
+    var currentPlayerTotalTurns = currentGame.currentPlayer.hitAmount + currentGame.currentPlayer.missAmount;
+    $("#currentPlayer").text("PLAYER_ONE" === currentGame.currentPlayer.playerType ?
+        (gameRecord.creator.username + "'s Turn") :
+        (gameRecord.participants[0]["username"] === gameRecord.creator.username ?
+            (gameRecord.participants[1]["username"]) : (gameRecord.participants[0]["username"])) + "'s Turn");
     $("#playerScore").text("Player Score: " + currentGame.currentPlayer.score);
-    $("#turnsCompleted").text("Turn Number: " + currentGame.currentPlayer.turnNumber);
+    $("#turnsCompleted").text("Total Turns: " + currentPlayerTotalTurns);
     $("#totalHits").text("Total Hits: " + currentGame.currentPlayer.hitAmount);
     $("#totalMisses").text("Total Misses: " + currentGame.currentPlayer.missAmount);
-    $("#timePerMove").text("Time Per Move: " + currentGame.currentPlayer.totalTurnTime);
+    var averageTimePerMove = currentGame.currentPlayer.totalTurnTime /
+        currentPlayerTotalTurns / 1000;
+    averageTimePerMove = (averageTimePerMove.toString() === "NaN") ? 0 : averageTimePerMove;
+    $("#timePerMove").text("Time Per Move: " + Math.round(averageTimePerMove) + " seconds");
     $("#minesLeft").text("Mines Left: " + currentGame.currentPlayer.amountOfMinesToPlace);
 }
 
@@ -200,12 +310,23 @@ function updateGeneralDataLine(gameRecord) {
     var currentGame = gameRecord.game;
     var tab = "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0";
     var currentTime = new Date().getTime();
+    var totalTurns = currentGame.players[0].hitAmount + currentGame.players[0].missAmount +
+        currentGame.players[1].hitAmount + currentGame.players[1].missAmount;
+    var timePassedInSec = Math.floor((currentTime - currentGame.startTime) / 1000);
+    var timeModulo = timePassedInSec%60 < 10 ? ("0" + timePassedInSec%60) : (timePassedInSec%60);
+    var totalTimeFormat;
+
+    if (timePassedInSec < 60){
+        totalTimeFormat = "0:" + timeModulo;
+    } else {
+        totalTimeFormat = Math.floor(timePassedInSec/60) + ":" + timeModulo;
+    }
+
     $("#generalData").text(gameRecord.participants[0]["username"] + "'s Score: " +
         currentGame.player2.score + tab +
         (gameRecord.participants.length === 1 ? "" : gameRecord.participants[1]["username"] + "'s Score: " +
-            gameRecord.game.player1.score + tab) +
-        "Time Passed: " + (currentTime - currentGame.startTime) / 1000 +
-        tab + "Total Turns Completed: " + (gameRecord.turnsAmount === undefined ? 0 : gameRecord.turnsAmount));
+            gameRecord.game.player1.score + tab) + "Time Passed: " + totalTimeFormat +
+        tab + "Total Turns Completed: " + totalTurns);
 }
 
 function updateOpponentName(participants) {
@@ -225,7 +346,7 @@ function updateOpponentName(participants) {
 function updateWatchersTable(watchers) {
     var table = "";
     for (var i = 0; i < watchers.length; i++) {
-        table += "<tr><td> " + watchers[i]["username"] + " </td></tr>";
+        table += "<tr><th>" + watchers[i]["username"] + "</th></tr>";
     }
     $("#watchersTable").text(table);
 }
